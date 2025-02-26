5293bc812babd31087d6bfd37888ea3c7e3e149e
Tolian500 <tolian500@gmail.com>
2025-02-26 13:18:55 +0100

    example done with common things

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 export default {
   data() {
     return {
-      name: 'Logistics'
+      name: 'Logistics',
+      status: 'active',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://www.google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
     }
   }
 }
@@ -10,4 +24,18 @@ export default {
 
 <template>
   <h1>Hello {{name}}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a :href="link">Google</a>
+
+  <br />
+
+  <button @:click="toggleStatus">Change Status</button>
 </template>
