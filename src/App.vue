44adb8197aaf159750297ed84b1181273877f181
Tolian500 <tolian500@gmail.com>
2025-02-26 13:57:27 +0100

    data import from webpage json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, onMounted} from 'vue';
 
 
 const name = ref('Logistics');
 const status = ref('active');
 const tasks = ref(['Task One', 'Task Two', 'Task Three']); 
+const newTask = ref(''); // add default value
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -14,9 +15,28 @@ const toggleStatus = () => {
   } else {
     status.value = 'active';
   }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
 }
 
+const deleteTask = (index) => {
+  tasks.value.splice(index,1)
+};
 
+onMounted(async () =>{
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.slice(0, 10).map((task) => task.title);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+  }
+});
 </script>
 
 <template>
@@ -25,13 +45,19 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">New Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Add Task</button>
+  </form>
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task,index) in tasks" :key="task">
+      <span> {{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
-
-
   <br />
-
-  <button @:click="toggleStatus">Change Status</button>
+  <button @click="toggleStatus">Change Status</button>
 </template>
